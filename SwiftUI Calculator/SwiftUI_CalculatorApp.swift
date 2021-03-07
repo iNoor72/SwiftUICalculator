b217ac7632362a5909cb4d317d1cf8c085e96abe
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-03-08 00:22:52 +0200

    Initial Commit

---
+++ SwiftUI Calculator/SwiftUI_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CalculatorApp.swift
+//  SwiftUI Calculator
+//
+//  Created by Noor Walid on 08/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
