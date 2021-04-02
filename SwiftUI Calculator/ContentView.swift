f48298abddcb72d8b5c447beff986eb643711c6d
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-04-02 20:49:08 +0200

    Initial commit, including the first design of the app.

--- SwiftUI Calculator/ContentView.swift
+++ SwiftUI Calculator/ContentView.swift
@@ -7,10 +7,46 @@
 
 import SwiftUI
 
+let operationArray = ["+","-","/","x","="]
+let numbersArray = [1,2,3,4,5,6,7,8,9]
+
+
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(.black).edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Result")
+                    .padding(.bottom, 160)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                HStack(spacing:32){
+                    VStack{
+                        HStack{ForEach(numbersArray, id: \.self) {number in
+                            Button("\(number)"){}
+                                .frame(width: 30, height: 50, alignment: .center)
+                                .foregroundColor(.white)
+                        }
+                        }
+                        
+                    }
+                    VStack{
+                        ForEach(operationArray, id: \.self) {op in
+                            Button(op){}
+                                .padding()
+                                .foregroundColor(.white)
+                        }
+                    }
+                    
+                }
+                
+                Button("0"){}
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50, alignment: .center)
+            }
+        }
     }
 }
 
