662e45e17dcb300a1eb1f8214c81c416f30a3c7a
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-04-04 14:12:30 +0200

    Better UI using colors and shapes.

--- SwiftUI Calculator/ContentView.swift
+++ SwiftUI Calculator/ContentView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 let operationArray = ["+","-","/","x","="]
-let numbersArray = [1,2,3,4,5,6,7,8,9]
-
+let numbersArray = [1,2,3]
+let numArray = [4,5,6]
+let numbers = [7,8,9]
 
 struct ContentView: View {
     
@@ -22,33 +23,62 @@ struct ContentView: View {
                     .foregroundColor(.white)
                     .padding()
                 
-                HStack(spacing:32){
+                HStack(spacing:16){
                     VStack{
-                        HStack{ForEach(numbersArray, id: \.self) {number in
-                            Button("\(number)"){}
-                                .frame(width: 30, height: 50, alignment: .center)
-                                .foregroundColor(.white)
+                        HStack{
+                            ForEach(numbersArray, id: \.self) {number in
+                                Button("\(number)"){}.frame(width: 30, height: 50, alignment: .center)
+                                    .foregroundColor(.white)
+                                    .background(Color.gray)
+                                    .clipShape(Circle())
+                            }.padding()
                         }
+                        HStack{
+                            ForEach(numArray, id: \.self) {number in
+                                Button("\(number)"){}.frame(width: 30, height: 50, alignment: .center)
+                                    .foregroundColor(.white)
+                                    .background(Color.gray)
+                                    .clipShape(Circle())
+                            }.padding()
+                            
+                        }
+                        HStack{
+                            ForEach(numbers, id: \.self) {number in
+                                Button("\(number)"){}.frame(width: 30, height: 50, alignment: .center)
+                                    .foregroundColor(.white)
+                                    .background(Color.gray)
+                                    .clipShape(Circle())
+                            }.padding()
+                            
                         }
                         
+                        Button("0"){}
+                            .foregroundColor(.white)
+                            .frame(width: 300, height: 50, alignment: .center)
+                            .background(Color.orange)
+                            .cornerRadius(16.0)
                     }
                     VStack{
                         ForEach(operationArray, id: \.self) {op in
                             Button(op){}
                                 .padding()
                                 .foregroundColor(.white)
+                                .background(Color.orange)
+                                .clipShape(Circle())
                         }
                     }
-                    
                 }
                 
-                Button("0"){}
-                    .foregroundColor(.white)
-                    .frame(width: 300, height: 50, alignment: .center)
             }
+            
         }
+        
+        
     }
+    
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
